2043cfc3c7eebd0686da5c0822c488ab89213223
Jackson Magnabosco <jacksonmagnabosco@globalsys.com.br>
2022-04-20 18:17:43 -0300

    add banco

---
+++ src/banco.js
@@ -0,0 +1,31 @@
+//https://www.youtube.com/watch?v=Irc3y08fFKg
+var amqp = require("amqplib");
+// Conectando com o rabbitmq
+const level = require("level-party");
+var db = level("../../db", { valueEncoding: "json" });
+// banco leveldb com multiplos niveis utilizando Round-Robin para escalonar as tarefas
+var helpers = require("../utils/helpers/helpers");
+
+amqp
+  .connect("amqp://localhost")
+  .then(function (conn) {
+    console.log("Conectado!");
+    return conn.createChannel();
+  })
+  .then(function (ch) {
+    console.log("Canal criado!");
+
+    ch.prefetch(1);
+    ch.consume('banco', function (msg) {
+      console.log("Requisição recebida %s", msg.properties.replyTo);
+      var dados = [];
+        db.createValueStream().on("data", function (data) {
+            console.log(data);
+            dados.push(data);
+        }).on("end", function () {
+          console.log("Terminou de buscar!");
+          ch.sendToQueue(msg.replyTo, helpers.JSONtoBuffer(dados));
+          ch.ack(msg);
+        });
+    });
+  });
